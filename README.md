29d012859333ed52345c0665d7ac48add78c84b2
cyberalex4life <cyberalex4life@gmail.com>
2018-04-11 16:26:53 +0300

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # argos-indicator-tlp
 Gnome Shell Indicator for TLP daemon, using Argos API
+
+#### Installation
+Install [Argos](https://extensions.gnome.org/extension/1176/argos/) Gnome-Shell extension.
+
+Then:
+```
+git clone https://github.com/cyberalex4life/argos-indicator-tlp.git
+cd argos-indicator-tlp
+cp -v tlp.r.3s.sh ~/.config/argos/
+sudo cp org.freedesktop.policykit.pkexec.tlp.policy /usr/share/polkit-1/actions/
+sudo cp pkroot /usr/local/bin/
+```
+**Note!**
+Further info to be added soon...
---
+++ tlp.r.3s.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+run_as_root="pkroot -d" # gksu; pkroot; gnomesu -c;
+this_script_path="$HOME/.config/argos/""$(basename "$0")"
+status=$(tlp stat -s  | grep Mode | awk '{ print $3 }' | tr [:lower:] [:upper:] )
+ac_icon="battery-full-charging-symbolic"
+battery_icon="battery-symbolic"
+get_status_icon_name(){
+	if [ "$status" = "BATTERY" ]
+	then
+        echo $battery_icon
+	elif [  "$status" = "AC" ]
+	then
+		echo $ac_icon
+	fi
+}
+
+_tlp="$(which tlp)"
+_tlp_ac="$_tlp ac"
+_tlp_bat="$_tlp bat"
+tlp_ac="\"$run_as_root \\\"$_tlp_ac\\\"; $this_tlp_script_path\""
+tlp_bat="\"$run_as_root \\\"$_tlp_bat\\\"; $this_tlp_script_path\""
+
+
+active_mode_icon=$(get_status_icon_name)
+
+status=$(tlp stat -s  | grep Mode | awk '{ print $3 }' | tr [:lower:] [:upper:] )
+governor_status=$(cpupower frequency-info  -p | grep -o -P '(?<=").*(?=")')
+echo "| iconName=$active_mode_icon"
+echo "---"
+echo "TLP Mode:"
+echo "$status | terminal=false iconName=$active_mode_icon"
+echo "---"
+echo "On Power | iconName=$ac_icon bash=$tlp_ac terminal=false"
+echo "On Battery | iconName=$battery_icon bash=$tlp_bat  terminal=false"
